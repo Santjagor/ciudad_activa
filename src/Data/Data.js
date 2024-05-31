3d9a5010001279b82e808918d853db6141f92d05
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-05-31 15:51:52 -0300

    Posts

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -18,51 +18,15 @@ export const sections = [
 
 export const blog = [
     {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
+        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
+        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...',
+        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
+        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
     },
     {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
-    },
-    {
-        title: "Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino",
-        description: "El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...",
-        image: "https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG",
-        url: "https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino",
+        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
+        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
+        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
+        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
     },
 ]
