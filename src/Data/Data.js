db8b58dbbde294e28a0a6198c783accccf687dfb
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-05-31 15:55:42 -0300

    post

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -19,7 +19,7 @@ export const sections = [
 export const blog = [
     {
         title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Arg...',
+        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
         image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
         url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
     },
