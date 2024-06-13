39be5007de514fc188b7b8680ead7402a550b30b
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-06-13 12:23:24 -0300

    main article

--- src/View/MainArticle/MainArticle.jsx
+++ src/View/MainArticle/MainArticle.jsx
@@ -4,7 +4,7 @@ export default function MainArticle() {
     return (
         <div className={style.container}>
             <div className={style.topImageContainer}>
-                <img className={style.topImage} src="/imagen-principal.jpg" alt="main-image" />
+                <img className={style.topImage} src="/banner-1.jpg" alt="banner" />
             </div>
             <div className={style.title}>GOBIERNO ABIERTO</div>
             <div className={style.title}>PARTICIPACION CIUDADANA</div>
@@ -21,8 +21,10 @@ export default function MainArticle() {
                     En un tiempo presente signado por diagnósticos críticos, sin esperanza ni aparente respuesta a los problemas
                     cotidianos que atraviesa nuestra sociedad, es tiempo de potenciar la inteligencia colectiva, para abordar la
                     complejidad de la democracia inmersa en una crisis, no solo global, sino estructural; donde sus cimientos han
-                    sido erosionados en los últimos veinte años, por una triada apocalíptica: Crisis política, crisis económica,
-                    crisis social.
+                    sido erosionados en los últimos veinte años, por una triada apocalíptica:
+                    <strong> Crisis política</strong>,
+                    <strong> crisis económica</strong>,
+                    <strong> crisis social</strong>.
                 </div>
                 <div className={style.text}>
                     Los ámbitos donde esta potencialidad, la de anteponer lo colectivo por sobre lo individual, se puede desarrollar
@@ -31,21 +33,21 @@ export default function MainArticle() {
                     para construir este tipo de experiencias. La desprivatización del Estado exige incrementar el control que la
                     sociedad ejerce sobre sus instituciones y recursos.
                 </div>
-                <div className={style.text}>
-                    Frente a la opacidad, el secretismo, el lenguaje críptico y las negociaciones bajo la mesa, proponemos la transparencia,
-                    la comunicación permanente, la clarificación de los intereses y actores en cada decisión pública, la conformación de
-                    espacios estables de control social con un mínimo nivel de especialización y conocimiento.
-                </div>
-                <div className={style.text}>
-                    Frente a la opacidad, el secretismo, el lenguaje críptico y las negociaciones bajo la mesa, proponemos la transparencia,
-                    la comunicación permanente, la clarificación de los intereses y actores en cada decisión pública, la conformación de
-                    espacios estables de control social con un mínimo nivel de especialización y conocimiento.
-                </div>
-                <div className={style.text}>
-                    El gobierno Radical de Miguel Lunghi pone en evidencia su carácter de gobierno cada vez más  cerrado. Todas las decisiones
-                    se toman a espaldas de la sociedad, que no participa ni de la formulación ni del control de las políticas públicas.
-                    Además, el desgaste  de 20 años de gestión está acompañado de pérdida de políticas estratégicas, falta de cuadros para la
-                    gestión y reina la improvisación.
+                <div className={style.box}>
+                    <img className={style.image} src="imagen-principal.jpg" alt="" />
+                    <div>
+                        <div className={style.text}>
+                            Frente a la opacidad, el secretismo, el lenguaje críptico y las negociaciones bajo la mesa, proponemos la transparencia,
+                            la comunicación permanente, la clarificación de los intereses y actores en cada decisión pública, la conformación de
+                            espacios estables de control social con un mínimo nivel de especialización y conocimiento.
+                        </div>
+                        <div className={style.text}>
+                            El gobierno Radical de Miguel Lunghi pone en evidencia su carácter de gobierno cada vez más  cerrado. Todas las decisiones
+                            se toman a espaldas de la sociedad, que no participa ni de la formulación ni del control de las políticas públicas.
+                            Además, el desgaste  de 20 años de gestión está acompañado de pérdida de políticas estratégicas, falta de cuadros para la
+                            gestión y reina la improvisación.
+                        </div>
+                    </div>
                 </div>
                 <div className={style.text}>
                     En sus  primeros años de gobierno, esta forma de hacer, sin interacción con la ciudadanía, quedó solapada por el gran
@@ -58,7 +60,7 @@ export default function MainArticle() {
                     vez mayores, la seguridad no tiene conducción política, es imposible acceder a una vivienda y al suelo urbano, y la falta de
                     empatía gubernamental hacia los reclamos de los vecinos es vergonzoso.
                 </div>
-                <div className={style.text}>
+                <div className={style.text_strong}>
                     Nosotros elegimos una democracia donde el pueblo delibere con sus representantes todo el tiempo, no solo a través de ellos.
                     Con un funcionamiento  cada vez más atento a las demandas de la sociedad.
                 </div>
@@ -68,16 +70,21 @@ export default function MainArticle() {
                     modelo de ciudad que queremos, de fuerte construcción de comunidad. Se trata de una política de largo plazo y estructuradora
                     de la gestión. No simplemente de un dispositivo de legitimación de la autoridad de turno.
                 </div>
-                <div className={style.text}>
-                    Podemos mencionar como esbozo actual de dispositivos de ciudadanía,  el foro de seguridad, o la mesa contra la violencia género:
-                    a ambos espacios los tenemos que potenciar, fortalecer y brindar más herramientas. Otros espacios a crear rápidamente si somos
-                    gobierno, es el presupuesto participativo, el concejo municipal de hábitat, la elección democrática de los delegados de Vela,
-                    Gardey, y otras delegaciones a crearse, audiencias públicas, obras públicas cooperativas para la integración sociourbana,
-                    lucha comunitaria de prevención de adicciones,  concejo municipal deportivo, mesa para la preservación de las sierras, etc.
-                    La democracia no se reduce al sufragio. El diálogo debe continuar después de las elecciones. Los vecinos deben ser parte de la
-                    elaboración y control de las políticas públicas. Más ciudadanía, es sinónimo de  mejores políticas públicas.
+                <div className={style.box}>
+                    <div>
+                        <div className={style.text}>
+                            Podemos mencionar como esbozo actual de dispositivos de ciudadanía,  el foro de seguridad, o la mesa contra la violencia género:
+                            a ambos espacios los tenemos que potenciar, fortalecer y brindar más herramientas. Otros espacios a crear rápidamente si somos
+                            gobierno, es el presupuesto participativo, el concejo municipal de hábitat, la elección democrática de los delegados de Vela,
+                            Gardey, y otras delegaciones a crearse, audiencias públicas, obras públicas cooperativas para la integración sociourbana,
+                            lucha comunitaria de prevención de adicciones,  concejo municipal deportivo, mesa para la preservación de las sierras, etc.
+                            La democracia no se reduce al sufragio. El diálogo debe continuar después de las elecciones. Los vecinos deben ser parte de la
+                            elaboración y control de las políticas públicas. Más ciudadanía, es sinónimo de  mejores políticas públicas.
+                        </div>
+                    </div>
+                    <img className={style.image} src="imagen-principal.jpg" alt="" />
                 </div>
-                <div className={style.text}>
+                <div className={style.text_strong}>
                     Las políticas públicas, no se pueden desarrollar a espaldas de la sociedad, sino escuchando e involucrándola todo el tiempo.
                 </div>
                 <div className={style.text}>
@@ -90,8 +97,9 @@ export default function MainArticle() {
                     la ampliación del sufragio universal, y se estrechó por arriba, dejando en pocas manos la toma de decisiones.
                 </div>
                 <div className={style.text}>
-                    Nadie se realiza en una comunidad que no se realiza, decía el General Perón y agregaba: ¨El problema del pensamiento
-                    democrático futuro está en resolvernos  a dar cabida en su paisaje a la comunidad…¨
+                    Nadie se realiza en una comunidad que no se realiza, decía el General Perón y agregaba: <strong>
+                        ¨El problema del pensamiento
+                        democrático futuro está en resolvernos  a dar cabida en su paisaje a la comunidad…¨</strong>
                 </div>
                 <div className={style.text}>
                     No hay justicia social y libertad sin protagonismo popular ni viceversa. Apuntar al bien común es y debe ser el objetivo compartido.
@@ -103,7 +111,7 @@ export default function MainArticle() {
                     “Ni la justicia social ni la libertad, motores de nuestro tiempo, son comprensibles en una comunidad montada sobre seres insectificados.
                     Nuestra comunidad, a la que debemos aspirar, es aquella donde la libertad y la responsabilidad son causa y efecto,
                     en que exista una alegría de ser, fundada en la persuasión de la dignidad propia. Una comunidad donde el individuo tenga realmente
-                    algo que ofrecer al bien general, algo que integrar y no solo su presencia muda y temerosa”
+                    algo que ofrecer al bien general, algo que integrar y no solo su presencia muda y temerosa.”
                 </div>
             </div>
         </div>
