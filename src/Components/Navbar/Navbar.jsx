33914a9ddaae763e9a422cc6b347ee0da01fed41
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-07-11 13:30:13 -0300

    nav buttons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import styles from './Navbar.module.css'
 import { sections } from '../../Data/Data'
+import { useLocation } from 'react-router-dom'
 
 export default function Navbar({ handleBlog, handleSections, navigate }) {
+    const location = useLocation()
     return (
         <div className={styles.container}>
             <div className={styles.ncContainer}>
                 <img onClick={() => { navigate('/') }} className={styles.logo} src="Logo2.png" alt="" />
             </div>
             <div className={styles.buttonContainer}>
-
                 {sections.map(section => {
                     return (
-                        <div id={section.id} onClick={handleSections} className={styles.button}>{section.name}</div>
+                        <div
+                            id={section.id}
+                            onClick={handleSections}
+                            className={location.pathname === `/${section.id}` ? styles.button_active : styles.button}
+                        >
+                            {section.name}
+                        </div>
                     )
                 })}
                 <a onClick={handleBlog} href="#blog">
