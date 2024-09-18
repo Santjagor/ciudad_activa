1039c6a6c70daa0045f5ad34817a76c7c25fae25
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-09-18 17:59:55 -0300

    data

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -23,16 +23,10 @@ export const sections = [
 
 export const blog = [
     {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
+        title: 'Los comedores populares juntarán firmas buscando que se trate una ordenanza encajonada',
+        description: '"En medio de una profundización de la crisis social, los responsables de los comedores barriales se presentarán en Rodríguez y Pinto para recolectar firmas...',
+        image: 'https://cdnartic.ar/KgPCYCH6Ad2jlPWS5d4QcBLICwc=/768x0/https://cdn.eleco.com.ar/media/2023/09/paola_quero.jpg',
+        url: 'https://www.eleco.com.ar/la-ciudad/los-comedores-populares-juntaran-firmas-buscando-que-se-trate-una-ordenanza-encajonada',
     },
     {
         title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
@@ -46,76 +40,5 @@ export const blog = [
         image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
         url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
     },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
-    {
-        title: 'Presentaron una resolución para rechazar el vaciamiento de la oficina del Correo Argentino',
-        description: 'El Bloque de Concejales de Unión por la Patria presentó un proyecto de resolución para rechazar el desmantelamiento de la oficina local del Correo Argentino.',
-        image: 'https://cdn.eleco.com.ar/media/2023/08/concejo_nicolas_carrillo.JPG',
-        url: 'https://www.eleco.com.ar/la-ciudad/presentaron-una-resolucion-para-rechazar-el-vaciamiento-de-la-oficina-del-correo-argentino',
-    },
-    {
-        title: 'El concejal Nicolás Carrillo presentó cuatro proyectos para el transporte público',
-        description: '"Las medidas de Milei están afectando a todos los sectores y el transporte es uno de ellos", indicó el edil de Unión por la Patria.',
-        image: 'https://www.lavozdetandil.com.ar/contenido/noticias/original/2024/03/07/1709817488.webp',
-        url: 'https://www.lavozdetandil.com.ar/nota-el-concejal-nicolas-carrillo-presento-cuatro-proyectos-para-el-transporte-publico-117028',
-    },
+    
 ]
