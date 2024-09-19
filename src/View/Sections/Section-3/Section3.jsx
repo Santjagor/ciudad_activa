3cbe3229472b0cbbb0fdc2d93efc01b11f0de475
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-09-19 12:01:19 -0300

    styles and pictures

--- src/View/Sections/Section-3/Section3.jsx
+++ src/View/Sections/Section-3/Section3.jsx
@@ -16,8 +16,9 @@ export default function Section() {
                 </div>
                 <div id='Inicio' className={style.title}>Vivienda e integración urbana</div>
                 <div className={style.articleContainer}>
-                    <div className={style.text}>Los datos del último censo reportan que Tandil evidencia el mas bajo nivel de acceso a la vivienda propia en la Provincia de Buenos Aires.</div>
+                    <div className={style.subtitle_2}>Los datos del último censo reportan que Tandil evidencia el mas bajo nivel de acceso a la vivienda propia en la Provincia de Buenos Aires.</div>
                     <div className={style.text}>La ordenanza Nª 9.865, Plan de Desarrollo Territorial, establece en su principio general, la creación de instrumentos redistributivos de la renta urbana. En dicha ordenanza su capítulo 1º procura asegurar respuestas a las necesidades de los ciudadanos en cuanto a la calidad de vida y la justicia social.</div>
+                    <img src="02.jpg" alt="02" className={style.image} />
                     <div className={style.text}>Promovemos la reducción de las desigualdades facilitando el acceso a la tierra urbana. Buscando impulsar procesos de mejora e integración socio urbana y  el acceso legal  de los sectores populares a la ciudad.</div>
                     <div className={style.text}>El PDT en su Capítulo III, establece los instrumentos de gestión y desarrollo, como parte del sistema municipal de gestión territorial, entre ellos en su Sección III, la creación del Fondo de Desarrollo Urbano, cuyos fondos deberían ser afectados y  determinados por ordenanza especial del Concejo deliberante a propuesta del Departamento ejecutivo. Cosa que lamentablemente nunca se cumplió.</div>
                     <div className={style.text}>En dicho apartado, establece  los destinos del Fondo de Desarrollo Urbano y los recursos que constituirán dicho fondo.</div>
