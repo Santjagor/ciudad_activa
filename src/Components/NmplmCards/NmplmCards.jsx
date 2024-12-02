9fee4b1865028fc1181c37fe89374f2a840f1252
Santiago Martin Solavaggione <santirigore@gmail.com>
2024-12-02 12:32:35 -0300

    nmplm

--- src/Components/NmplmCards/NmplmCards.jsx
+++ src/Components/NmplmCards/NmplmCards.jsx
@@ -4,7 +4,7 @@ import { nmplm } from '../../Data/Data'
 export default function NmplmCards({ handleSections }) {
     return (
         <div className={styles.nmplmContainer}>
-            <img src="NMPLM2_1.PNG" alt="NMPLM2_1" className={styles.nmplm_titleImage} />
+            <img src="NMPLM2_1.png" alt="NMPLM2_1" className={styles.nmplm_titleImage} />
             <div className={styles.nmplmButtonContainer}>
                 {nmplm.map(m => {
                     return (
